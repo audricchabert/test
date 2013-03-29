d455a904194226d08665449da530b5bc3745a39b
Mazen Gharbi <magharbi@polytech.unice.fr>
2013-03-29 13:32:40 +0100

    TTAN-62: Console => respect des conventions java

--- src/main/java/fr/unice/polytech/si3/tse/ttan/vues/Console.java
+++ src/main/java/fr/unice/polytech/si3/tse/ttan/vues/Console.java
@@ -20,41 +20,55 @@ public class Console {
 
 	public void lancer() {
 		// On va simuler 1 tour de plateau !
-		System.out.println("Debut du tour");
 		
-		Joueur j1 = new Joueur("Joueur 1"); // On cree notre joueur
+		/* NOS STRING */
+		String chaineLarve = " larve / ";
+		String chaineNourrice = " nourrices / ";
+		String chaineOuvrieres = " ouvrieres";
+		
+		// On cree notre joueur
+		Joueur j1 = new Joueur("Joueur 1"); 
 		PlateauIndividuel plateau = j1.getPlateauIndividuel();
 		
-		System.out.println(j1.getPseudo() + " => " + plateau.getLarves() + " larve / "+plateau.getNourrices() + " nourrices / "+ plateau.getOuvrieres()  + " ouvrieres");
+		/* TEST
+		System.out.println("Debut du tour");
+		
+		System.out.println(j1.getPseudo() + " -> " + plateau.getLarves() + chaineLarve  +plateau.getNourrices() + chaineNourrice + plateau.getOuvrieres()  + chaineOuvrieres);
 		
 		// PHASE EVENEMENT
+		
 		System.out.println("\nPhase evenements !");
 		System.out.println("Evenement actuel : " + Evenements.getInstance().getEvenementCourant());
 		System.out.println(j1.getPseudo()+ " decalle l'evenement de 1 vers la droite");
 		plateau.decallerEvenement(1, Constantes.DROITE);
 		System.out.println("Le nouvel evenement est : " + Evenements.getInstance().getEvenementCourant());
-		System.out.println(j1.getPseudo() + " => " + plateau.getLarves()  + " larve / " + plateau.getNourrices()  + " nourrices / " + plateau.getOuvrieres()  + " ouvrieres");
+		System.out.println(j1.getPseudo() + " =====> " + plateau.getLarves()  + chaineLarve + plateau.getNourrices()  + chaineNourrice + plateau.getOuvrieres()  + chaineOuvrieres);
 		
 		// PHASE NAISSANCE
 		System.out.println("\nPhase naissance !");
 		System.out.println(j1.getPseudo()+" place une nourrice dans l'atelier sur l'evenement 'AMELIORER FOURMILIERE'");
 		plateau.placerNourriceAtelier(Constantes.AMELIORER_FOURMILIERE);
-		System.out.println(j1.getPseudo() + " => " + plateau.getLarves()  + " larve / " + plateau.getNourrices()  + " nourrices / " + plateau.getOuvrieres()  + " ouvrieres");
+		System.out.println(j1.getPseudo() + " > " + plateau.getLarves()  + chaineLarve + plateau.getNourrices()  + chaineNourrice + plateau.getOuvrieres()  + chaineOuvrieres);
 		System.out.println(j1.getPseudo()+" cree une ouvriere");
-		plateau.naissanceOuvrieres(2); // Creer 1 ouvriere en utilisant 2 nourrices
-		System.out.println(j1.getPseudo() + " => " + plateau.getLarves()  + " larve / " + plateau.getNourrices()  + " nourrices / " + plateau.getOuvrieres()  + " ouvrieres");
+		
+		// Creer 1 ouvriere en utilisant 2 nourrices
+		plateau.naissanceOuvrieres(2); 
+		System.out.println(j1.getPseudo() + " ===> " + plateau.getLarves()  + chaineLarve + plateau.getNourrices()  + chaineNourrice + plateau.getOuvrieres()  + chaineOuvrieres);
 		
 		// OUVRIERES
-		System.out.println("\nPhase ouvrieres !");
+		System.out.println("\nPhase "+chaineOuvrieres+" !");
 		System.out.println(j1.getPseudo()+" place une ouvriere sur le niveau 0 de la fourmillere");
 		plateau.placerOuvriereFourmiliere(0);
 		System.out.println(j1.getPseudo()+" recupere 1 larve ");
 		plateau.genereRessourceFourmiliere();
-		System.out.println(j1.getPseudo() + " => " + plateau.getLarves()  + " larve / " + plateau.getNourrices()  + " nourrices / " + plateau.getOuvrieres()  + " ouvrieres");
+		System.out.println(j1.getPseudo() + " ==> " + plateau.getLarves()  + chaineLarve + plateau.getNourrices()  + chaineNourrice + plateau.getOuvrieres()  + chaineOuvrieres);
 	
 		// AMELIORER 
 		System.out.println("\nAmeliorer fourmiliere");
 		plateau.ameliorerFourmiliere();
+		
+		//*/
+	
 	}
 	
 }
